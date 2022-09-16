ca4ebae788348be4d20afb914f5fa3a83a944c3d
oxyvans <benja.1999.g@hotmail.com>
2022-09-16 14:26:13 -0300

    fix h1 in back

--- src/components/skills/Back.jsx
+++ src/components/skills/Back.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Back = () => {
   return (
     <div className='skills__content-'>
-        <h3 className='skills__title-'>Frontend Developer</h3>
+        <h3 className='skills__title-'>Backend Developer</h3>
 
         <div className='skills__box-'>
             <div className='skills__group-'>
